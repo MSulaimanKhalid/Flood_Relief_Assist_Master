e1b3651f1d68545e8054040c00f9bc8628c5fc51
Muhammad Sulaiman Khalid <msulaimank.parhyar@gmail.com>
2025-12-04 20:14:09 +0500

    main name update

--- src/main/java/org/example/zereaab/Sulaiman_Test_Main.java
+++ src/main/java/org/example/zereaab/Sulaiman_Test_Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Sulaiman_Test_Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("eps.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Sulaiman_Test_Main.class.getResource("eps.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Evacuation Path Recommender");
         stage.setScene(scene);
