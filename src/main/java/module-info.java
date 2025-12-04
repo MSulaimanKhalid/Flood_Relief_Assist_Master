26df2e531c8625f641f0cbb22d068bd793caeca2
Muhammad Sulaiman Khalid <msulaimank.parhyar@gmail.com>
2025-12-04 15:16:09 +0500

    maps strated working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,12 @@ module org.example.zereaab {
     requires javafx.fxml;
     requires com.gluonhq.maps;
     requires javafx.controls;
-    requires javafx.graphics;
     requires mapsforge.map;
+    requires mapsforge.core;
+    requires mapsforge.map.awt;
+    requires mapsforge.themes;
+    requires mapsforge.map.reader;
+    requires javafx.swing;
 
 
     opens org.example.zereaab to javafx.fxml;
--- src/main/java/org/example/zereaab/HelloApplication.java
+++ src/main/java/org/example/zereaab/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("evacuatePathScreen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("eps.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Evacuation Path Recommender");
         stage.setScene(scene);
